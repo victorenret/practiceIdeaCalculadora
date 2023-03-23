2beb4fb6c7789598d57c55c3ccf18a4f2151cccf
Victor Retamal <victor.retamal@xintec.cl>
2023-03-23 12:55:27 -0300

    configuring connection to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CalculadoraController } from './calculadora/calculadora.controller';
 import { CalculadoraService } from './calculadora/calculadora.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [CalculadoraController],
   providers: [CalculadoraService],
 })
