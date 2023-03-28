1f990c5aa963f092967e9dfab8b47968c0a6f211
Victor Retamal <victor.retamal@xintec.cl>
2023-03-28 17:28:58 -0300

    add createDivi and Linter

--- src/calculadora/entities/division.entity.ts
+++ src/calculadora/entities/division.entity.ts
@@ -1,20 +1,21 @@
-import { IsNumber } from 'class-validator';
+import { IsNumber, IsOptional } from 'class-validator';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity()
 export class Division {
-  @PrimaryColumn()
+  @PrimaryColumn('uuid')
   id: string;
 
   @IsNumber()
   @Column()
   n1: number;
 
-  @Column()
   @IsNumber()
+  @Column()
   n2: number;
 
-  @Column()
   @IsNumber()
-  resultado: number;
+  @IsOptional()
+  @Column('numeric', { default: 0 })
+  resultado?: number;
 }
